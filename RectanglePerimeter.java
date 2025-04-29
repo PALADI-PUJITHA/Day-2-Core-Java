a45d369eb550b6a105cc00f3f822ca88246551b2
PALADI-PUJITHA <paladipujitha@gmail.com>
2025-04-29 23:19:41 +0530

    Add files via upload

---
+++ RectanglePerimeter.java
@@ -0,0 +1,11 @@
+public class RectanglePerimeterCalculator
+{
+	public static void calculatePerimeter(double l,double b)	
+	{
+		System.out.println("Perimeter"+2*(l+b));
+	}
+	public static void main(String args[])
+	{
+		calculatePerimeter(5.0,3.0);
+	}
+}
